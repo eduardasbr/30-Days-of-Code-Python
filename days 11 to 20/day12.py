fbef410c5c53c99e132f51b7993a2e42b22b198c
Eduarda Brito <britoeduarda.br19@gmail.com>
2025-10-12 21:21:33 -0300

    day 12 exercise

---
+++ days 11 to 20/day12.py
@@ -0,0 +1,17 @@
+import os
+os.system("cls")
+
+# DIA 12
+# Peça uma nota e diga se o aluno foi aprovado (≥ 6).
+
+print(""" ========= SCHOOL GRADES SYSTEM =========
+
+""")
+
+grade = float(input("Student's grade: "))
+
+if grade >= 6:
+    print("Passed!")
+else:
+    print("Failed!")
+
